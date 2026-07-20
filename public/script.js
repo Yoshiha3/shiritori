5d658a2bd6019e14650c2feeedb5a42e30545b85
Yoshiha <haru12292997@gmail.com>
2026-07-20 14:39:46 +0900

    feat: リクエストの関数化

--- public/script.js
+++ public/script.js
@@ -1,30 +1,63 @@
 window.onload = async () => {
-  const response = await fetch("/shiritori", { method: "GET" });
-  const { previousWord } = await response.json();
-  const paragraph = document.querySelector("#previousWord");
-  paragraph.innerHTML = `前の単語: ${previousWord}`;
+  try {
+    const { previousWord } = await get("/shiritori");
+    const paragraph = document.querySelector("#previousWord");
+    paragraph.innerHTML = `前の単語: ${previousWord}`;
+  } catch (_e) {
+    alert("通信エラーが発生しました。");
+    return;
+  }
 };
 
 document.querySelector("#nextWordSendButton").onclick = async () => {
   const nextWordInput = document.querySelector("#nextWordInput");
   const nextWordInputText = nextWordInput.value;
-  const response = await fetch(
-    "/shiritori",
-    {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ nextWord: nextWordInputText }),
-    },
-  );
-
-  if (response.status !== 200) {
-    const errorObj = await response.json();
-    alert(errorObj["errorMessage"]);
+  try {
+    const data = await post("/shiritori", { nextWord: nextWordInputText });
+    const paragraph = document.querySelector("#previousWord");
+    paragraph.innerHTML = `前の単語: ${data.previousWord}`;
+    nextWordInput.value = "";
+  } catch (e) {
+    if (e instanceof ApiError) {
+      alert(e.message);
+    } else {
+      alert("通信エラーが発生しました。");
+    }
     return;
   }
-
-  const { previousWord } = await response.json();
-  const paragraph = document.querySelector("#previousWord");
-  paragraph.innerHTML = `前の単語: ${previousWord}`;
-  nextWordInput.value = "";
 };
+
+async function request(url, options = {}) {
+  const response = await fetch(url, options);
+
+  const data = await response.json();
+
+  if (!response.ok) {
+    throw new ApiError(response.status, data.errorMessage);
+  }
+
+  return data;
+}
+
+class ApiError extends Error {
+  constructor(status, message) {
+    super(message);
+    this.status = status;
+  }
+}
+
+function get(url) {
+  return request(url, {
+    method: "GET",
+  });
+}
+
+function post(url, body) {
+  return request(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(body),
+  });
+}
